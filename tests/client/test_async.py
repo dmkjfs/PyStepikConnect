510f96186147b2fa9d262308d179caf09b285713
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-15 10:47:06 +0300

    Update tests

--- tests/client/test_async.py
+++ tests/client/test_async.py
@@ -1,5 +1,7 @@
 import pytest
 
+from unittest.mock import AsyncMock
+
 from pystepikconnect.client.asynchronous import AsyncStepik
 from pystepikconnect.exceptions import AuthorizationError
 
@@ -12,6 +14,13 @@ def test_auth(client_id: str) -> None:
 @pytest.mark.asyncio
 async def test_get_courses(async_stepik: AsyncStepik) -> None:
     assert isinstance(await async_stepik.get_courses(), list)
+
+
+@pytest.mark.asyncio
+async def test_get_sections(async_stepik: AsyncStepik) -> None:
+    courses = await async_stepik.get_courses()
+    course = courses[0]
+    assert isinstance(await async_stepik.get_sections(course_id=course.id), list)
 
 
 @pytest.mark.asyncio
@@ -29,3 +38,53 @@ async def test_get_units(async_stepik: AsyncStepik) -> None:
     lessons = await async_stepik.get_lessons()
     lesson = lessons[0]
     assert isinstance(await async_stepik.get_units(lesson_id=lesson.id, course_id=lesson.courses[0]), list)
+
+
+@pytest.mark.asyncio
+async def test_create_course(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(async_stepik.create_course(course=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_update_course(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.update_course(course=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_create_section(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.create_section(section=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_update_section(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.update_section(section=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_create_lesson(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.create_lesson(lesson=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_update_lesson(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.update_lesson(lesson=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_create_unit(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.create_unit(unit=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_update_unit(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.update_unit(unit=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_create_step(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.create_step(step=await async_mock), int)
+
+
+@pytest.mark.asyncio
+async def test_update_step(async_stepik: AsyncStepik, async_mock: AsyncMock) -> None:
+    assert isinstance(await async_stepik.update_step(step=await async_mock), int)
--- tests/client/test_sync.py
+++ tests/client/test_sync.py
@@ -1,5 +1,7 @@
 import pytest
 
+from unittest.mock import Mock
+
 from pystepikconnect.client.synchronous import SyncStepik
 from pystepikconnect.exceptions import AuthorizationError
 
@@ -11,6 +13,11 @@ def test_auth(client_id: str) -> None:
 
 def test_get_courses(stepik: SyncStepik) -> None:
     assert isinstance(stepik.get_courses(), list)
+
+
+def test_get_sections(stepik: SyncStepik) -> None:
+    course = stepik.get_courses()[0]
+    assert isinstance(stepik.get_sections(course_id=course.id), list)
 
 
 def test_get_lessons(stepik: SyncStepik) -> None:
@@ -25,3 +32,43 @@ def test_get_units(stepik: SyncStepik) -> None:
     lessons = stepik.get_lessons()
     lesson = lessons[0]
     assert isinstance(stepik.get_units(lesson_id=lesson.id, course_id=lesson.courses[0]), list)
+
+
+def test_create_course(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.create_course(course=mock), int)
+
+
+def test_update_course(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.update_course(course=mock), int)
+
+
+def test_create_section(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.create_section(section=mock), int)
+
+
+def test_update_section(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.update_section(section=mock), int)
+
+
+def test_create_lesson(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.create_lesson(lesson=mock), int)
+
+
+def test_update_lesson(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.update_lesson(lesson=mock), int)
+
+
+def test_create_unit(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.create_unit(unit=mock), int)
+
+
+def test_update_unit(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.update_unit(unit=mock), int)
+
+
+def test_create_step(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.create_step(step=mock), int)
+
+
+def test_update_step(stepik: SyncStepik, mock: Mock) -> None:
+    assert isinstance(stepik.update_step(step=mock), int)
