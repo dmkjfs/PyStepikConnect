87a399725d879aaf60c7f15d5011c45aba8fb25c
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-16 13:48:48 +0300

    Add exceptions

--- pystepikconnect/exceptions.py
+++ pystepikconnect/exceptions.py
@@ -1,10 +1,24 @@
-from logging import getLogger
-
-
-logger = getLogger("pystepikconnect")
-
-
 class AuthorizationError(Exception):
     """
     Incorrect credentials - `client_id` or `client_secre`
+    """
+
+
+class NotFoundError(Exception):
+    """
+    Status code 404
+    Not found
+    """
+
+
+class ForbiddenError(Exception):
+    """
+    Status code 403
+    Not enough permissions
+    """
+
+
+class AuthorPermissionError(ForbiddenError):
+    """
+    Not enough permissions
     """
