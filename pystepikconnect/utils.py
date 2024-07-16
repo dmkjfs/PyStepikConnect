b44604b9740cfc7424089c249972b20372ba119a
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-17 02:12:39 +0300

    Fix

--- pystepikconnect/utils.py
+++ pystepikconnect/utils.py
@@ -1,2 +1,5 @@
-def dict_to_query(params: dict) -> str:
+from typing import Union
+
+
+def dict_to_query(params: Union[dict, None]) -> str:
     return "?" + "&".join([f"{param}={value}" for param, value in params.items()]) if params is not None else ''
