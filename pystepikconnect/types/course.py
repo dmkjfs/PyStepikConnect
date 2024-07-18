831c9950dc00332c253c4ba26c66ee522e339561
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-18 15:51:46 +0300

    Update course model

--- pystepikconnect/types/course.py
+++ pystepikconnect/types/course.py
@@ -4,11 +4,12 @@ from typing import Optional
 
 class Course(BaseModel):
     id: Optional[int] = None
-    summary: str
-    intro: str
-    workload: str
-    course_format: str
-    description: str
+    title: str
+    summary: Optional[str]
+    intro: Optional[str]
+    workload: Optional[str]
+    course_format: Optional[str]
+    description: Optional[str]
     total_units: Optional[int] = None
     first_lesson: Optional[int] = None
     first_unit: Optional[int] = None
