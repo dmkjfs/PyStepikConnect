9199a52cdd9a7b27e6c1ca564a12c3f6f20cb068
Ilya Kuznetsov <ilkztsff@gmail.com>
2024-07-18 16:30:31 +0300

    Update utils

--- pystepikconnect/utils.py
+++ pystepikconnect/utils.py
@@ -1,5 +1,24 @@
 from typing import Union
 
+from pydantic import BaseModel
+
 
 def dict_to_query(params: Union[dict, None]) -> str:
+
+    """
+    Transforms dictionary with query params into query string
+
+    :param params:
+    """
+
     return "?" + "&".join([f"{param}={value}" for param, value in params.items()]) if params is not None else ''
+
+
+def model_to_dict(model: BaseModel) -> dict:
+    """
+    Transforms Pydantic model into dictionaty and removes None items
+
+    :param model: pydantic model you need to transform
+    """
+
+    return {key: item for key, item in model.model_dump().items() if item is not None}
